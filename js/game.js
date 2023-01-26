d68742e374521f82d4d390bde01ea34d81e8a8a8
Samuel Golovin <samuel.samyco@gmail.com>
2023-01-26 17:06:23 -0600

    remaking the boxes because of mouse and canvas

--- js/game.js
+++ js/game.js
@@ -1,79 +1,18 @@
-window.addEventListener('load', function() {
-    const canvas = document.getElementById('myCanvas');
-    const ctx = canvas.getContext('2d');
-    canvas.width = 540;
-    canvas.height = 400;
+window.onload = function() {
+    function startGame() {
+        var gameArea = document.getElementById("game-area");
 
-    class InputHandler {
-        constructor(game) {
-            this.game = game;
-            window.addEventListener("click", e => {
-                if (e.target == canvas) {
-                    this.game.mouseClicked = e;
-                    console.log(this.game.mouseClicked);
-                    if (e.target == this.box) {
-                        console.log(e.target);
-                        this.box.pop();
-                    }
-                }
-                
-            })
+        boxCount = 10;
+        for(var i = 0; i < boxCount; i++) {
+            var box = document.createElement("div");
+            box.innerHTML = "";
+            box.className = "box";
+            box.style.left = 2 + parseInt(Math.random() * 646) + "px";
+		    box.style.top = 2 + parseInt(Math.random() * 246) + "px";
+            //box.addEventListerener("mouseup", boxWhenClicked);
+            gameArea.appendChild(box);
         }
     }
 
-    var size = 50;
-    var numOfBoxes = 5;
-    var boxes = [];
-
-    class Box {
-        constructor(game, posX, posY, size) {
-            this.game = game;
-            this.width = size;
-            this.height = size;
-            this.x = posX;
-            this.y = posY;
-        }
-        draw(context) {
-            context.fillRect(this.x, this.y, this.width, this.height);
-        }
-        update() {
-
-        }
-    }
-
-    class Game {
-        constructor(width, height) {
-            this.width = width;
-            this.height = height;
-            this.mouseClicked = undefined;
-            this.input = new InputHandler(this);
-            for (var i = 0; i < numOfBoxes; i++) {
-                let x = Math.floor(Math.random() * (this.width - size));
-                let y = Math.floor(Math.random() * (this.height - size));
-                this.box = new Box(this, x, y, size);
-                boxes.push(this.box);
-                console.log(this.box);
-                console.log(boxes);
-            }
-            
-        }
-        render(context) {
-            for(var i = 0; i < numOfBoxes; i++) {
-                boxes[i].draw(context);
-                boxes[i].update(context);
-            }
-        }
-        
-    }
-
-    const game = new Game(canvas.width, canvas.height);
-    game.render(ctx);
-    
-    function animate() {
-        //ctx.clearRect(0, 0, canvas.width, canvas.height);
-        game.render(ctx);
-        game.box.draw(ctx);
-        requestAnimationFrame(animate);
-    }
-    animate();
-});
+    startGame();
+};
