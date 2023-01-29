1623a8235748ee025a52b4a8d92f0f9d7cd95d3b
Samuel Golovin <samuel.samyco@gmail.com>
2023-01-28 18:28:46 -0600

    boxes now load onto the game screen in random pos

--- js/game.js
+++ js/game.js
@@ -1,18 +1,48 @@
-window.onload = function() {
-    function startGame() {
-        var gameArea = document.getElementById("game-area");
+//This is the javascript that controls the game
+//Thanks for checking out my code!
+//Samuel Golovin
 
-        boxCount = 10;
+//Variables that control aspects of the game are make the game easier to manipulate and tweak
+var boxSize = 50;           //size of the boxes
+var boxCount = 20;
+
+
+window.onload = function() {
+    function spawnBoxes() {
+        //gets the game canvas and assigns it to gameCanvas variable
+        var gameCanvas = document.getElementById("game-area-canvas");
+
+        //destroys boxes if any are still there
+        gameCanvas.innerHTML = "";
         for(var i = 0; i < boxCount; i++) {
+
+            //creates a new box game element
             var box = document.createElement("div");
-            box.innerHTML = "";
             box.className = "box";
-            box.style.left = 2 + parseInt(Math.random() * 646) + "px";
-		    box.style.top = 2 + parseInt(Math.random() * 246) + "px";
-            //box.addEventListerener("mouseup", boxWhenClicked);
-            gameArea.appendChild(box);
+
+            //places boxes in random positions on the game canvas
+            box.style.left = parseInt(Math.random() * (640 - boxSize)) + "px";
+            box.style.top = parseInt(Math.random() * (480 - boxSize)) + "px";
+            box.style.backgroundColor = "#000000";
+            box.style.width = boxSize + "px";
+            box.style.height = boxSize + "px";
+
+            //makes it so that when a box is clicked, a function is called that does something to it
+            box.addEventListener("mouseup", boxClicked);
+
+            //add the game element to the game canvas
+            gameCanvas.appendChild(box);
+
+            //used this to figure out why the boxes wouldn't spawn
+            console.log(box);
+            //was because I need to give them a width and height
+            //they were there, just couldn't see them :/
         }
     }
 
-    startGame();
+    function boxClicked() {
+        console.log("Box clicked");
+    }
+
+    spawnBoxes();
 };
